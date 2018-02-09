f627dca889206864824a9f3c01d5cb1af30e008b
Carl Emil Hattestad <carlis@cstickmedia.com>
2018-02-09 22:18:43 +0100

    change to return geoJson

--- project/mongo_db.py
+++ project/mongo_db.py
@@ -1,8 +1,8 @@
 from apistar import Component
-from pymongo import MongoClient, DESCENDING
-# from bson import ObjectId
-from apistar.types import Settings
 from apistar.http import Response
+from apistar.types import Settings
+from bson import ObjectId
+from pymongo import MongoClient
 
 
 class Database(Component):
@@ -10,11 +10,13 @@ class Database(Component):
     MongoDB class that holds all methods for interact with mongo.
     """
 
-    def __init__(self, uri, db, uni_coll, country_coll):
+    def __init__(self, uri: str, db: str, uni_coll: str, country_coll: str) -> None:
         """
+        Creates connections to the database
         :param uri:  Uri for mongodb e.g "mongodb://localhost:27017/gib"
-        :param db: the database in mongodb
-        :param coll: the collection in the database
+        :param db: the database in mongodb, e.g. 'gib'
+        :param uni_coll: the university collection in the database, e.g. 'uni'
+        :param country_coll: the country collection in the database, e.g. 'world_countries'
         """
         self._mongo = MongoClient(uri)
         self._db = self._mongo[db]
@@ -38,47 +40,58 @@ class Database(Component):
             return q
         return wrapper
 
-    def ping(self):
+    def ping(self) -> bool:
         """
-        test if the app has connection to mongo
+        Pings the database
         :return: Bool
         """
-        ping = False
         try:
-            self._db.command('ping')
-            ping = True
+            self._db.command('ping')  # this method will timeout if no connection found
+            return True
         except:
-            pass
-        return ping
+            return False
 
-    @_serialize_object_id
-    def get_all_uni(self):
+    def get_university_by_id(self, _id: str):
         """
-        returns every uni doc in the collection
-        :return: list with uni docs
+        returns the university the _id match if found in database, else []
+        :param _id: string, hex
+        :return: dict
         """
-        q = list(self._uni_coll.find({}))
+        q = self._uni_coll.find_one({'_id': ObjectId(_id)})
+        q['_id'] = str(q['_id'])
         return q
 
     @_serialize_object_id
-    def list_all_uni(self, key):
+    def list_all_uni(self) -> list:
         """
         returns queries with id and key e.g key=universitet
         :param key: mongodb field name
-        :return:  list of queries with key
+        :return:  Cursor
         """
-        q = list(self._uni_coll.find({}, {key: 1}))
+        q = list(self._uni_coll.aggregate([{'$match': {'_id': {'$exists': True}}},
+                                          {'$project': {
+                                              'type': 'Feature',
+                                              'properties.university': '$universitet',
+                                              'properties._id': '$_id',
+                                              'geometry': '$geometry'
+                                          }}]))
         return q
 
     @_serialize_object_id
-    def search_by_all(self, text):
+    def search_by_all(self, text: str) -> list:
+        """
+        Returns regex match on text in the database
+        :param text: string of search
+        :return: list of universities
+        """
         q = list(self._uni_coll.find({'$text': {'$search': text}},
-                                 {'score': { '$meta': "textScore" }}
-                                 ))
+                                     {'score': { '$meta': "textScore" }}
+                                     ))
         return q
 
     @_serialize_object_id
     def get_country_list(self, country):
+        # TODO
         count = self._country_coll.find_one({'properties.name': {'$regex': country,
                                                                  '$options': 'i'
                                                                  }},
@@ -86,6 +99,12 @@ class Database(Component):
         q = list(self._uni_coll.find({'location': { '$geoWithin': { '$geometry' : count[
             'geometry']}}}))
 
+        return q
+
+    @_serialize_object_id
+    def get_fagomraader(self, search: str=None) -> list:
+        # TODO
+        q = self._uni_coll.distinct('Fagområde')
         return q
 
 
--- project/routes.py
+++ project/routes.py
@@ -4,17 +4,18 @@ from apistar.handlers import docs_urls, static_urls
 from project import views
 
 routes = [
-    # Route('/', 'GET', views.welcome),
+    Route('/ping_database', 'GET', views.ping_database, name='ping'),
 
-    Route('/mongo_ok', 'GET', views.mongo_ok),
+    Route('/get_university_by_id/{_id}', 'GET', views.get_university_by_id),
 
-    Route('/uni/{key}', 'GET', views.list_all_uni),
-
-    Route('/uni/all', 'GET', views.get_all_uni),
+    Route('/list_all_uni_as_geo_json', 'GET', views.list_all_uni_as_geo_json,
+          name='list_all_uni_as_geo_json'),
 
     Route('/search_by_all/{text}', 'GET', views.search_by_all),
 
     Route('/uni_in_country/{country}', 'GET', views.uni_in_country),
+
+    Route('/fagomraade', 'GET', views.get_fagomraader, name='get_fagomraader'),
 
     Include('/', docs_urls),
     Include('/static', static_urls)
